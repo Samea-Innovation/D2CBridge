59ca853faeea374441f840b693d54be990f3409b
ijaglin <ijaglin@samea-innovation.com>
2024-07-16 12:10:26 +0200

    Modified CombainResponse & CombainRequest variables to Objects

--- src/main/java/com/combain/model/CombainRequest.java
+++ src/main/java/com/combain/model/CombainRequest.java
@@ -50,14 +50,14 @@ public class CombainRequest {
     public static class CellTower {
         //      Parameter                   Required    Values          Description
         String radioType;               //  recommended gsm             The radio type of this cell tower. Can also be put directly in root JSON element if all cellTowers have same radioType
-        int mobileCountryCode;          //  true        0..999          The Mobile Country Code (MCC)
-        int mobileNetworkCode;          //  true        0..999          The Mobile Network Code (MNC)
-        int locationAreaCode;           //  true        0..65535        The Location Area Code (LAC)
-        int cellId;                     //  true        0..65535        The Cell ID (CID)
-        int signalStrength;             //  recommended -120--25        The measured signal strength for this cell tower in dBm
-        int timingAdvance;              //  false       0..63           The timing advance value for this cell tower when available
-        int primaryScramblingCode;      //  recommended 0..511          The primary scrambling code for WCDMA and physical CellId for LTE
-        boolean serving;                //  false       0..1            Specify with 0/1 if the cell is serving or not. If not specified, the first cell in the array is assumed to be serving
+        Integer mobileCountryCode;      //  true        0..999          The Mobile Country Code (MCC)
+        Integer mobileNetworkCode;      //  true        0..999          The Mobile Network Code (MNC)
+        Integer locationAreaCode;       //  true        0..65535        The Location Area Code (LAC)
+        Integer cellId;                 //  true        0..65535        The Cell ID (CID)
+        Integer signalStrength;         //  recommended -120--25        The measured signal strength for this cell tower in dBm
+        Integer timingAdvance;          //  false       0..63           The timing advance value for this cell tower when available
+        Integer primaryScramblingCode;  //  recommended 0..511          The primary scrambling code for WCDMA and physical CellId for LTE
+        Boolean serving;                //  false       0..1            Specify with 0/1 if the cell is serving or not. If not specified, the first cell in the array is assumed to be serving
     }
 
     @Data
@@ -66,9 +66,9 @@ public class CombainRequest {
         String macAddress;              //  true        ""              The mac address also called BSSID of the Wi-Fi router
         String ssid;                    //  recommended ""              The SSID of the Wi-Fi router
         String ssidHex;                 //  recommended ""              The SSID of the Wi-Fi network in a hex string format. Each byte of the SSID is represented by two HEX characters
-        int signalStrength;             //  recommended -100--20        The measured signal strength for the access point in dBm
-        int frequency;                  //  false       ?               The frequency of the Wi-Fi access point in MHz. Improves indoor location accuracy
-        int channel;                    //  false       ?               The channel measured of the Wi-Fi access point
+        Integer signalStrength;         //  recommended -100--20        The measured signal strength for the access point in dBm
+        Integer frequency;              //  false       ?               The frequency of the Wi-Fi access point in MHz. Improves indoor location accuracy
+        Integer channel;                //  false       ?               The channel measured of the Wi-Fi access point
         String age;                     //  false       ""              Time ago the measurement was made in ms
     }
 
@@ -77,56 +77,56 @@ public class CombainRequest {
         //      Parameter                   Required    Values          Description
         String macAddress;              //  true        ""              The mac address of the Bluetooth beacon
         String name;                    //  recommended ""              The name of the Bluetooth beacon
-        int signalStrength;             //  recommended -100--20        The measured signal strength for the bluetooth beacon in dBm
+        Integer signalStrength;             //  recommended -100--20        The measured signal strength for the bluetooth beacon in dBm
         String age;                     //  false       ""              Time ago the measurement was made in ms
         String uuid;                    //  false       ""              The uuid identifier of the beacon if iBeacon
-        int major;                      //  false       0..65535        The major identifier of the beacon if iBeacon
-        int minor;                      //  false       0..65535        The minor identifier of the beacon if iBeacon
+        Integer major;                  //  false       0..65535        The major identifier of the beacon if iBeacon
+        Integer minor;                  //  false       0..65535        The minor identifier of the beacon if iBeacon
     }
 
     @Data
     public static class Gps {
         //      Parameter                   Required    Values          Description
-        float latitude;                 //  true        -90..90         The GPS latitude
-        float longitude;                //  true        -180..180       The GPS longitude
-        int accuracy;                   //  true        0..255          The GPS accuracy (horizontal, CEP = 50%) in meters
-        int sat;                        //  recommended 0..20           The number of satellites seen in the measurement
-        int age;                        //  recommended 0..600          The age of the GPS location (in seconds)
-        int altitude;                   //  recommended -1000..10000    The altitude of the GPS location
-        int altitudeAccuracy;           //  false       0..100          The accuracy of the altitude in meters for the GPS location
-        int heading;                    //  recommended 0..360          The direction (bearing) of movement for the GPS location in degrees
-        int speed;                      //  recommended 0..100          The speed of the GPS location in m/s
+        Float latitude;                 //  true        -90..90         The GPS latitude
+        Float longitude;                //  true        -180..180       The GPS longitude
+        Integer accuracy;               //  true        0..255          The GPS accuracy (horizontal, CEP = 50%) in meters
+        Integer sat;                    //  recommended 0..20           The number of satellites seen in the measurement
+        Integer age;                    //  recommended 0..600          The age of the GPS location (in seconds)
+        Integer altitude;               //  recommended -1000..10000    The altitude of the GPS location
+        Integer altitudeAccuracy;       //  false       0..100          The accuracy of the altitude in meters for the GPS location
+        Integer heading;                //  recommended 0..360          The direction (bearing) of movement for the GPS location in degrees
+        Integer speed;                  //  recommended 0..100          The speed of the GPS location in m/s
     }
 
     // These parameters should be used with care since it could potentially return wrong coordinates. Associative array with values 1 for on and 0 for off. Parameters for turning individual fallbacks on or off
     @Data
     public static class Fallback {
         //      Parameter                   Required    Values          Description
-        boolean all;                    //  false       0..1            Enable/disable all fallbacks for this request. (This does not include cidxf which has to be activated manually)
-        boolean w4f;                    //  false       0..1            Used to support mobile units only able to read a 4 CID Hex digits of a WCDMA CID (normally 8 CID Hex digits). Search the CID but just compare the 4 less significant hex digits
-        boolean lacf;                   //  false       0..1            If the CID is not found, return the center and accuracy for the LAC
-        boolean nbcidf;                 //  false       0..1            Neighbouring CID. If the cells is not found, it will search for a slightly similar CID
-        boolean rncidf;                 //  false       0..1            If the cell is not found, search with MCC, MNC, RNCID (first part of CellId)
-        boolean mncf;                   //  false       0..1            If the MNC is not found, search for just MCC, LAC and CID
-        boolean ratf;                   //  false       0..1            If the cell is not found search different radio types
-        boolean cidf;                   //  false       0..1            If the cell is not found, search with just MCC, MNC, CID
+        Boolean all;                    //  false       0..1            Enable/disable all fallbacks for this request. (This does not include cidxf which has to be activated manually)
+        Boolean w4f;                    //  false       0..1            Used to support mobile units only able to read a 4 CID Hex digits of a WCDMA CID (normally 8 CID Hex digits). Search the CID but just compare the 4 less significant hex digits
+        Boolean lacf;                   //  false       0..1            If the CID is not found, return the center and accuracy for the LAC
+        Boolean nbcidf;                 //  false       0..1            Neighbouring CID. If the cells is not found, it will search for a slightly similar CID
+        Boolean rncidf;                 //  false       0..1            If the cell is not found, search with MCC, MNC, RNCID (first part of CellId)
+        Boolean mncf;                   //  false       0..1            If the MNC is not found, search for just MCC, LAC and CID
+        Boolean ratf;                   //  false       0..1            If the cell is not found search different radio types
+        Boolean cidf;                   //  false       0..1            If the cell is not found, search with just MCC, MNC, CID
     }
 
     @Data
     public static class LastLocation {
         //      Parameter                   Required    Values          Description
-        float lat;                      //  true        -90..90         The last known latitude
-        float lng;                      //  true        -180..180       The last known longitude
-        int accuracy;                   //  true        0..255          The accuracy of last known location in meters
-        int timestamp;                  //  true        timestamp       Unix timestamp
+        Float lat;                      //  true        -90..90         The last known latitude
+        Float lng;                      //  true        -180..180       The last known longitude
+        Integer accuracy;               //  true        0..255          The accuracy of last known location in meters
+        Integer timestamp;              //  true        timestamp       Unix timestamp
     }
 
     @Data
     public static class Sensor {
         //      Parameter                   Required    Values          Description
-        int pressure;                   //  false       ?               The ambient air pressure in hPa or mbar. Normally 1013 at sea level
-        int steps;                      //  false       ?               The number of steps since last request. For best performance use effective number of steps (=distance). Thus, if going 4 steps straight and then 3 steps to the right, the effective number of steps are 5
-        int heading;                    //  false       0..359          The heading of the movement in degrees
+        Integer pressure;               //  false       ?               The ambient air pressure in hPa or mbar. Normally 1013 at sea level
+        Integer steps;                  //  false       ?               The number of steps since last request. For best performance use effective number of steps (=distance). Thus, if going 4 steps straight and then 3 steps to the right, the effective number of steps are 5
+        Integer heading;                //  false       0..359          The heading of the movement in degrees
         String activity;                //  false       ""              The detected activity in an Android device. Valid values are IN_VEHICLE, ON_BICYCLE, ON_FOOT, RUNNING, STILL, TILTING, UNKNOWN and WALKING
     }
 }
--- src/main/java/com/combain/model/CombainResponse.java
+++ src/main/java/com/combain/model/CombainResponse.java
@@ -5,12 +5,12 @@ import lombok.Data;
 @Data
 public class CombainResponse {
     Location location;          //  Array of the estimated location. See below for details.
-    int accuracy;               //  The accuracy is the estimated median error in meters, i.e. the radius in a circle with 50% confidence level.
+    Integer accuracy;               //  The accuracy is the estimated median error in meters, i.e. the radius in a circle with 50% confidence level.
 
-    float heading;              //  Heading for GPS location if available.
-    float altitude;             //  Altitude for GPS location if available.
-    float speed;                //  Speed for GPS location if available (m/s).
-    int calculatedSpeed;        //  Calculated speed between this and previous location (m/s).
+    Float heading;              //  Heading for GPS location if available.
+    Float altitude;             //  Altitude for GPS location if available.
+    Float speed;                //  Speed for GPS location if available (m/s).
+    Integer calculatedSpeed;        //  Calculated speed between this and previous location (m/s).
 
     Geoname geoname;            //  If geoname is set in the request the geoname object will be returned if available. See below for details.
     Indoor indoor;              //  If indoor is set in the request the indoor object will be returned if available. See below for details.
@@ -19,12 +19,14 @@ public class CombainResponse {
     String fallback;            //  If this field is included no direct match was found. It shows which fallback was used to generate the response.
     String state;               //  If this field is included, it should preferably be used in next request for the specific device. The parameter includes positioning history that enhances indoor positioning performance and enables speed filter.
 
-    long logId;                 //  A logId of the request that could be used for debugging purposes. Requests are stored for maximum 7 days currently and can be downloaded in portal.
+    Error error;                //  In the case of an error, a standard format error response body will be returned and the HTTP status code will be set to an error status.
+
+    Long logId;                 //  A logId of the request that could be used for debugging purposes. Requests are stored for maximum 7 days currently and can be downloaded in portal.
 
     @Data
     public static class Location {
-        float lat;              //  The latitude of the estimated location.
-        float lng;              //  The longitude of the estimated location.
+        Float lat;              //  The latitude of the estimated location.
+        Float lng;              //  The longitude of the estimated location.
     }
 
     @Data
@@ -36,10 +38,10 @@ public class CombainResponse {
 
     @Data
     public static class Indoor {
-        int buildingModelId;    //  The ID of the building model used to calculate the location.
-        int buildingId;         //  The ID of the building that the used building model belongs to.
+        Integer buildingModelId;    //  The ID of the building model used to calculate the location.
+        Integer buildingId;         //  The ID of the building that the used building model belongs to.
         String building;        //  The name of the building that the used building model belongs to.
-        int floorIndex;         //  The index of the calculated floor if available. 0 means ground level.
+        Integer floorIndex;         //  The index of the calculated floor if available. 0 means ground level.
         String floorLabel;      //  The name of the calculated floor if available.
         String room;            //  The name of the calculated room if available.
     }
